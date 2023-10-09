e753d4c76a17c4b3c299921edfec9cc95a3e11f8
nselpriv <nicolaiselo@gmail.com>
2023-10-09 15:04:03 +0200

    6.2

--- Assignment 5/week5/HigherFun.fs
+++ Assignment 5/week5/HigherFun.fs
@@ -30,6 +30,8 @@ let rec lookup env x =
 type value = 
   | Int of int
   | Closure of string * string * expr * value env       (* (f, x, fBody, fDeclEnv) *)
+  | Clos of string * expr * value env (* (x,body,declEnv) *)
+
 
 let rec eval (e : expr) (env : value env) : value =
     match e with
@@ -65,7 +67,8 @@ let rec eval (e : expr) (env : value env) : value =
         let xVal = eval eArg env
         let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
         in eval fBody fBodyEnv
-      | _ -> failwith "eval Call: not a function";;
+      | _ -> failwith "eval Call: not a function"
+    | Fun(x, body) -> Clos(x, body, env)
 
 (* Evaluate in empty environment: program must have no free variables: *)
 
